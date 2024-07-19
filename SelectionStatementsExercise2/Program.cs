80066930446f4cc534e4b5b283a20db2133dc3eb
snajjar69 <snajjar69@gmail.com>
2024-07-19 12:37:37 -0400

    added Statement 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -1,10 +1,31 @@
 ﻿namespace SelectionStatementsExercise2
 {
-    public class Program
+    class Program
     {
         static void Main(string[] args)
+        
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite School subject?");
+            var subject = Console.ReadLine();
+            
+            switch (subject)
+            { 
+                case "Math":
+                    Console.WriteLine("Math is too hard of a subject!");
+                    break;
+                case "Science":
+                    Console.WriteLine("Science is a hard subject!");
+                    break;
+                case "History":
+                    Console.WriteLine("History Class is very interesting subject!");
+                    break;
+                default:
+                    Console.WriteLine($"Oh Wow! I never took this subject before. {subject} sounds fun!");
+                    break;
+                }
+            }
+            
         }
+        
     }
-}
+    
